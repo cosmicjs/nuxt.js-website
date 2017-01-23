18d8ff52daf12298e4c2bd60b75ba71603229820
Tony Spiro <tspiro@tonyspiro.com>
2017-01-22 20:08:07 -0600

    added yarn

--- plugins/cosmic.js
+++ plugins/cosmic.js
@@ -1,12 +1,10 @@
-import config from '../config'
+import config from '~/config'
 import Cosmic from 'cosmicjs'
 
 export default {
   getPage (slug) {
     const data = new Promise((resolve, reject) => {
-      console.log('getPage')
       Cosmic.getObject(config.cosmic, { slug }, (err, res) => {
-        console.log(err, res)
         if (err) return reject(err)
         resolve(res)
       })
