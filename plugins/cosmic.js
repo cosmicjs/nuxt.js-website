c0b37f058624c603c8f268064aaabd689ba8fba4
Tony Spiro <tspiro@tonyspiro.com>
2017-01-22 19:32:11 -0600

    error test

--- plugins/cosmic.js
+++ plugins/cosmic.js
@@ -4,7 +4,9 @@ import Cosmic from 'cosmicjs'
 export default {
   getPage (slug) {
     const data = new Promise((resolve, reject) => {
+      console.log('getPage')
       Cosmic.getObject(config.cosmic, { slug }, (err, res) => {
+        console.log(err, res)
         if (err) return reject(err)
         resolve(res)
       })
